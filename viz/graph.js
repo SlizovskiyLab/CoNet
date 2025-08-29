78c093b78c7c19063b6c6cfc9a5fe9459f4598e5
suhashidesilva <suhashi.desilva@gmail.com>
2025-08-29 14:24:51 -0400

    fix arrow head color, add legends

--- viz/graph.js
+++ viz/graph.js
@@ -18,3 +18,29 @@ document.getElementById("collapseArrow").addEventListener("click", function() {
 });
 
 
+const tooltip = d3.select("#tooltip");
+
+const link = g.selectAll("path.link")
+  .data(data.links)
+  .join("path")
+  .attr("class", "link")
+  .attr("stroke", d => d.color || "#999")
+  .attr("marker-end", d => d.isColo ? null : `url(#arrow-${(d.color || "#999").replace("#","")})`)
+  .attr("stroke-width", d => Math.max(1, d.penwidth || 1))
+  .attr("stroke-dasharray", d => d.isColo ? null : "4 2")
+  .on("mouseover", (event, d) => {
+    if (d.type === "colocalization") {
+      tooltip.style("display", "block")
+             .html(`Individual count: <b>${d.individualCount}</b>`);
+    }
+  })
+  .on("mousemove", (event) => {
+    tooltip.style("left", (event.pageX + 10) + "px")
+           .style("top", (event.pageY + 10) + "px");
+  })
+  .on("mouseout", () => {
+    tooltip.style("display", "none");
+  });
+
+
+
