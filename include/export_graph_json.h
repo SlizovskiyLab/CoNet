7fbc38c670a69bc707676ba05e818aca92fb68ad
Sakesh Andhavarapu <a.sakesh@gmail.com>
2025-09-19 12:03:24 -0400

    json disease filtering

--- include/export_graph_json.h
+++ include/export_graph_json.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <string>
+#include <map>
 #include "graph.h"
 
+bool exportGraphToJsonSimple(const Graph& g, const std::string& outPathStr, const std::map<int, std::string>& patientToDiseaseMap);
 
-bool exportGraphToJsonSimple(const Graph& g, const std::string& outPathStr);
-bool exportParentGraphToJson(const Graph& g, const std::string& outPathStr, bool showLabels=true);
+bool exportParentGraphToJson(const Graph& g, const std::string& outPathStr, const std::map<int, std::string>& patientToDiseaseMap, bool showLabels = true);
